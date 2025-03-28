8bc70d0589eac55ec0e8d14352b1b33a5c5f2e3b
Dishant <dishantphandyal04@gmail.com>
2025-03-28 22:40:47 +0530

    dishant's server

--- server.js
+++ server.js
@@ -40,3 +40,19 @@ app.post("/addUser", async (req, res) => {
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
 });
+
+/*node server.js
+
+for mongoDB
+docker run -d -p27017:27017 --name mongo --network mongo-network -e MONGO_INITDB_ROOT_USERNAME=admin -eMONGO_INITDB_ROOT_PASSWORD=qwerty mongo
+
+for mongo-express
+docker run -d -p8081:8081 --name mongo-express --network mongo-network -e ME_CONFIG_MONGODB_ADMINUSERNAME=admin -e ME_CONFIG_MONGODB_ADMINPASSWORD=qwerty -e ME_CONFIG_MONGODB_URL="mongodb://admin:qwerty@mongo:27017" mongo-express
+
+
+
+
+
+
+
+*/
